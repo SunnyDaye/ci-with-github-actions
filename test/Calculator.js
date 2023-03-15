d8dba24eaebac6480514f7086fc88245648a631d
Suncerie Daye <SunnyDaye@users.noreply.github.com>
2023-03-15 04:03:30 +0000

    More test and chai added

--- test/Calculator.js
+++ test/Calculator.js
@@ -1,6 +1,12 @@
 //load node.js assertion library
 const assert = require("assert");
 
+//load chai assertion library
+const chai = require("chai");
+const expect = chai.expect;
+const asserttype = require('chai-asserttype');
+chai.use(asserttype);
+
 //load calculator code
 var Calculator = require("../Calculator");
 var calc = new Calculator();
@@ -9,22 +15,34 @@ describe("Calculator Tests", function() {
 
   it("add: Adds 1 and 2", function() {
     var result = calc.add(1, 2);
-    assert.equal(result, 3);
+    expect(result).to.be.equal(3);
+  });
+
+  it("Add Allows one parameter", function() {
+    expect(calc.add(1)).to.be.equal(1);
+  });
+
+  it("Add Allows no parameters", function() {
+    expect(calc.add()).to.be.equal(0);
+  });
+
+  it("Add turns strings to zeros", function() {
+    expect(calc.add("frog", "toad")).to.be.number();
   });
 
   it("subtract: Subtracts 6 and 3", function() {
     var result = calc.subtract(6, 3);
-    assert.equal(result, 3);
+    expect(result).to.be.equal(3);
   });
 
   it("multiply: Multiplies 3 and 7", function() {
     var result = calc.multiply(3, 7);
-    assert.equal(result, 21);
+    expect(result).to.be.equal(21);
   });
 
   it("divide: Divides 4 and 2", function() {
     var result = calc.divide(4, 2);
-    assert.equal(result, 2);
+    expect(result).to.be.equal(2);
   });
 
 });
